df7caa940bd992b177b71c9058c673be29c8e5fb
Thomas Müller <mimmi20@live.de>
2015-07-20 22:50:54 +0200

    initial commit

---
+++ src/WurflConstants.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright (c) 2015 ScientiaMobile, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Refer to the COPYING.txt file distributed with this package.
+ *
+ *
+ * @category   WURFL
+ * @package    WURFL
+ * @copyright  ScientiaMobile, Inc.
+ * @license    GNU Affero General Public License
+ */
+
+namespace Wurfl;
+
+/**
+ * WURFL PHP API Constants
+ *
+ * @package WURFL
+ */
+class WurflConstants
+{
+    const GENERIC             = 'generic';
+    const GENERIC_XHTML       = 'generic_xhtml';
+    const GENERIC_WEB_BROWSER = 'generic_web_browser';
+    const GENERIC_MOBILE      = 'generic_mobile';
+
+    const MEMCACHE     = 'memcache';
+    const APC          = 'apc';
+    const FILE         = 'file';
+    const NULL_CACHE   = 'null';
+    const EACCELERATOR = 'eaccelerator';
+    const SQLITE       = 'sqlite';
+    const MYSQL        = 'mysql';
+
+    const NO_MATCH      = null;
+    const RIS_DELIMITER = '---';
+}
